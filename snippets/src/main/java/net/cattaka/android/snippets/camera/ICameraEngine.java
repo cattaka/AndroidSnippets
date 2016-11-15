e131ba6cb6ce062ec277e8b3ca74b909c14e28a0
Takao Sumitomo <cattaka@mail.cattaka.net>
2016-11-15 11:37:51 +0900

    Add CaptureTarget

---
+++ snippets/src/main/java/net/cattaka/android/snippets/camera/ICameraEngine.java
@@ -0,0 +1,11 @@
+package net.cattaka.android.snippets.camera;
+
+import android.view.TextureView;
+
+/**
+ * Created by cattaka on 16/11/15.
+ */
+
+public interface ICameraEngine {
+    void addPreviewTarget(TextureView textureView);
+}

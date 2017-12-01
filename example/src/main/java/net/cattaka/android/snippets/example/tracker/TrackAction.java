5faf5e3c0802a8d2739097cd6f07a137399b354f
Takao Sumitomo <cattaka@mail.cattaka.net>
2017-12-01 15:30:24 +0900

    Add TrackAction

---
+++ example/src/main/java/net/cattaka/android/snippets/example/tracker/TrackAction.java
@@ -0,0 +1,21 @@
+package net.cattaka.android.snippets.example.tracker;
+
+/**
+ * NODE: Follow the limitation of Firebase Analytics, such as num of actions, don't use space in action name.
+ */
+public enum TrackAction {
+    ACTION_CLICK("action_click"),
+    ACTION_LONG_CLICK("action_long_click"),
+    //
+    ;
+    public final String code;
+
+    TrackAction(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return code;
+    }
+}

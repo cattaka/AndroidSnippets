bf72702d4e88d11a85f1a51d9f8118c2820f045d
Takao Sumitomo <cattaka@mail.cattaka.net>
2018-09-21 11:25:25 +0900

    Add MotionLayoutMorphActivity

--- example/src/main/java/net/cattaka/android/snippets/example/MainActivity.java
+++ example/src/main/java/net/cattaka/android/snippets/example/MainActivity.java
@@ -27,7 +27,8 @@ public class MainActivity extends AppCompatActivity implements IScreen {
     private static final List<ActivityEntry> ACTIVITY_ENTRIES = Arrays.asList(
             new ActivityEntry("MotionLayout", null,
                     new ActivityEntry("Stretchable Surfaces", MotionLayoutStretchableSurfacesActivity.class, Build.VERSION_CODES.JELLY_BEAN_MR2),
-                    new ActivityEntry("Switch Scene", MotionLayoutSwitchSceneActivity.class, Build.VERSION_CODES.JELLY_BEAN_MR2)
+                    new ActivityEntry("Switch Scene", MotionLayoutSwitchSceneActivity.class, Build.VERSION_CODES.JELLY_BEAN_MR2),
+                    new ActivityEntry("Morph", MotionLayoutMorphActivity.class, Build.VERSION_CODES.JELLY_BEAN_MR2)
             ),
             new ActivityEntry("With Google Applications", null,
                     new ActivityEntry("Pick from Google Photos", PickFromGooglePhotosActivity.class)
---
+++ example/src/main/java/net/cattaka/android/snippets/example/MotionLayoutMorphActivity.java
@@ -0,0 +1,15 @@
+package net.cattaka.android.snippets.example;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import net.cattaka.android.snippets.example.tracker.IScreen;
+
+public class MotionLayoutMorphActivity extends AppCompatActivity implements IScreen {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion_layout_morph);
+    }
+}

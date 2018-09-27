87acbeb6a5d0e97b34653ad61b3094b67e2bc822
Takao Sumitomo <cattaka@mail.cattaka.net>
2018-09-27 14:47:17 +0900

    Upgrade MotionLayoutSwitchSceneActivity

--- example/src/main/java/net/cattaka/android/snippets/example/MotionLayoutSwitchSceneActivity.java
+++ example/src/main/java/net/cattaka/android/snippets/example/MotionLayoutSwitchSceneActivity.java
@@ -3,9 +3,12 @@ package net.cattaka.android.snippets.example;
 import android.os.Bundle;
 import android.support.constraint.motion.MotionLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import net.cattaka.android.snippets.example.tracker.IScreen;
+
+import java.util.Locale;
 
 /**
  * Created by takao on 2016/10/28.
@@ -14,6 +17,7 @@ import net.cattaka.android.snippets.example.tracker.IScreen;
 public class MotionLayoutSwitchSceneActivity extends AppCompatActivity implements View.OnClickListener, IScreen {
     private MotionLayoutSwitchSceneActivity me = this;
     private MotionLayout mMotionLayout;
+    private int mDirtyHackForSceneId = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,7 @@ public class MotionLayoutSwitchSceneActivity extends AppCompatActivity implement
 
         // Find views
         mMotionLayout = findViewById(R.id.motionLayout);
+        findViewById(R.id.button_center).setOnClickListener(this);
         findViewById(R.id.button_top).setOnClickListener(this);
         findViewById(R.id.button_start_bottom).setOnClickListener(this);
         findViewById(R.id.button_end_bottom).setOnClickListener(this);
@@ -31,15 +36,29 @@ public class MotionLayoutSwitchSceneActivity extends AppCompatActivity implement
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.button_top) {
-            mMotionLayout.setTransition(R.id.scene_center, R.id.scene_top);
-            mMotionLayout.setProgress(0);
+
+        Log.d("cattaka", String.format(Locale.ROOT, "%d:%f", mMotionLayout.getCurrentState(), mMotionLayout.getProgress()));
+        if (view.getId() == R.id.button_center) {
+            switchToState(R.id.scene_c);
+        } else if (view.getId() == R.id.button_top) {
+            switchToState(R.id.scene_t);
         } else if (view.getId() == R.id.button_start_bottom) {
-            mMotionLayout.setTransition(R.id.scene_center, R.id.scene_start_bottom);
-            mMotionLayout.setProgress(0);
+            switchToState(R.id.scene_sb);
         } else if (view.getId() == R.id.button_end_bottom) {
-            mMotionLayout.setTransition(R.id.scene_center, R.id.scene_end_bottom);
-            mMotionLayout.setProgress(0);
+            switchToState(R.id.scene_eb);
         }
     }
+
+    private void switchToState(int nextSceneId) {
+        // FIXME: In some case, getCurrentState() returns -1
+        int lastSceneId = mMotionLayout.getCurrentState();
+        if (lastSceneId == -1) {
+            lastSceneId = mDirtyHackForSceneId;
+        } else {
+            mDirtyHackForSceneId = lastSceneId;
+        }
+        mMotionLayout.setTransition(lastSceneId, nextSceneId);
+        mMotionLayout.setProgress(0);
+    }
+
 }

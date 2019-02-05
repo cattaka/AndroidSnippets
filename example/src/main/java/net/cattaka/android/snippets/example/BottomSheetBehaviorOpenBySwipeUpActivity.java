40795d33ecda090022dabaf87ff054886009b1e3
Takao Sumitomo <cattaka@mail.cattaka.net>
2019-02-05 16:44:04 +0900

    Handle swipe up

--- example/src/main/java/net/cattaka/android/snippets/example/BottomSheetBehaviorOpenBySwipeUpActivity.java
+++ example/src/main/java/net/cattaka/android/snippets/example/BottomSheetBehaviorOpenBySwipeUpActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 
 import net.cattaka.android.adaptertoolbox.adapter.ScrambleAdapter;
@@ -16,11 +17,34 @@ import net.cattaka.android.snippets.example.tracker.IScreen;
  */
 
 public class BottomSheetBehaviorOpenBySwipeUpActivity extends AppCompatActivity implements IScreen {
+    static String TAG = "BottomSheetBehaviorOpenBySwipeUpActivity";
+
     CoordinatorLayout mCoordinatorLayout;
     View mBottomSheetView;
+    View mTouchDelegateView;
 
-    ScrambleAdapter<String> mAdapter;
     BottomSheetBehavior mBottomSheetBehavior;
+
+    View.OnTouchListener mOnTouchListener = new View.OnTouchListener() {
+        float mLastY;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (mBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
+                float dy = event.getY() - mLastY;
+                mLastY = event.getY();
+                if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+                    mLastY = event.getY();
+                } else if (event.getActionMasked() == MotionEvent.ACTION_MOVE) {
+                    if (dy < 0) {
+                        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    }
+                }
+            }
+            mBottomSheetBehavior.onTouchEvent(mCoordinatorLayout, mBottomSheetView, event);
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,10 +54,12 @@ public class BottomSheetBehaviorOpenBySwipeUpActivity extends AppCompatActivity 
         // Find views
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.layout_coordinator);
         mBottomSheetView = findViewById(R.id.view_bottom_sheet);
+        mTouchDelegateView = findViewById(R.id.touch_delegate);
 
         // Bind event handlers
-
         mBottomSheetBehavior = CoordinatorLayoutUtils.pickBehavior(mBottomSheetView, BottomSheetBehavior.class);
         mBottomSheetBehavior.setHideable(true);
+
+        mTouchDelegateView.setOnTouchListener(mOnTouchListener);
     }
 }
